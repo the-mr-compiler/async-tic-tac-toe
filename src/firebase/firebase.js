5b49591bae44731f5bff5c7b345ac139d9de0005
Meghanath Nalawade <50794140+the-mr-compiler@users.noreply.github.com>
2022-12-23 12:57:17 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA202_8Rt0XxosJsQE87aA6yTUOINN9OcA",
-  authDomain: "tic-tac-toe-851bb.firebaseapp.com",
-  projectId: "tic-tac-toe-851bb",
-  storageBucket: "tic-tac-toe-851bb.appspot.com",
-  messagingSenderId: "192874917372",
-  appId: "1:192874917372:web:c8de964c7a23f13a341679",
+   apiKey: "YOUR_API_KEY",
+   authDomain: "YOUR_AUTH_DOMAIN",
+   projectId: "YOUR_PROJECT_ID",
+   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+   appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
